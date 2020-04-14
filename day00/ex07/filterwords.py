cdeffa1eb9ff656d08a29e422118c7ff2c0a47ab
evandrosutil <evandrosutil@gmail.com>
2020-04-13 22:04:32 -0300

    ex07

---
+++ day00/ex07/filterwords.py
@@ -0,0 +1,23 @@
+import string
+import sys
+
+if len(sys.argv) != 3:
+    print('ERROR')
+    exit()
+phrase = sys.argv[1]
+for i in phrase:
+    if i.isdigit():
+        print('ERROR')
+        exit()
+try:
+    length = int(sys.argv[2])
+except ValueError:
+    print('ERROR')
+
+result = [
+    word.strip(string.punctuation)
+    for word in phrase.split(' ')
+    if len(word) > length
+]
+
+print(result)
