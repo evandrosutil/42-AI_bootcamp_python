60e887a41b3d936d37b2434ca35909522a242cac
evandrosutil <evandrosutil@gmail.com>
2020-04-29 19:07:12 -0300

    d01e02: add tests

---
+++ day01/ex02/test_vector.py
@@ -0,0 +1,11 @@
+import unittest
+
+from vector import Vector
+
+
+class VectorTestCase(unittest.TestCase):
+
+    def create_vector_with_list(self):
+
+        vector_ex = Vector([0.0, 0.1, 0.2])
+        self.assert
